0b1d105faf676e037bb69c9588be6ec6eb2d7069
shanshal <ahmedmahdii2003@gmail.com>
2025-09-12 23:18:16 +0300

    Deleted old assets

--- image_processing/utils/distorter.py
+++ image_processing/utils/distorter.py
@@ -1,48 +1,69 @@
 import os
+import random
+from wand.image import Image
+import cv2
 import numpy as np
-from wand.image import  Image
 
-
-input_folder = ""
-output_folder = ""
-
-
-def radial_distort(distortion_amounts, input_path, output_path):
-    """
-    distortion_amounts = (0.9, 0.4, 0.2, 1.0)
-    """
-    with Image(filename = input_path) as img:
+def radial(input_path, output_path):
+    distortion_amounts = (random.uniform(0.8, 1.2),
+                          random.uniform(0.0, 0.5),
+                          random.uniform(0.0, 0.5),
+                          1.0)
+    with Image(filename=input_path) as img:
         img.virtual_pixel = 'white'
         img.distort('barrel', distortion_amounts)
         img.save(filename=output_path)
-# NOT IMPLEMENTED YET
-def elastic_distort():
-    """
-    Random elastic deformation
-    """
-#
 
-def stretch_image_wand(input_path, output_path, scale_x=1.0, scale_y=1.0):
-    """
-    scales are floats :>
-    """
+def stretch(input_path, output_path):
+    scale_x = random.uniform(0.8, 1.5)
+    scale_y = random.uniform(0.8, 1.5)
     with Image(filename=input_path) as img:
-        new_width = int(img.width * scale_x)
-        new_height = int(img.height * scale_y)
-        img.resize(width=new_width, height=new_height)
+        img.resize(int(img.width * scale_x), int(img.height * scale_y))
         img.save(filename=output_path)
-def affine_warp(input_path, output_path, src_points, dst_points):
-    """
-    src_points & dst_points: List of 3 [x,y] points in the original AND the output image
-    """
+
+def rotate(input_path, output_path):
+    angle = random.uniform(-45, 45)
+    with Image(filename=input_path) as img:
+        img.virtual_pixel = 'white'
+        img.rotate(angle)
+        img.save(filename=output_path)
+
+def translate(input_path, output_path, shift_x=0, shift_y=0):
+    with Image(filename=input_path) as img:
+        img.virtual_pixel = 'white'
+        # affine transform for translation: x' = x + shift_x, y' = y + shift_y
+        args = [0, 0, shift_x, shift_y,
+                img.width, 0, img.width + shift_x, shift_y,
+                0, img.height, shift_x, img.height + shift_y]
+        img.distort('affine', tuple(args))
+        img.save(filename=output_path)
+
+
+def scale(input_path, output_path):
+    image = cv2.imread(input_path)
+    new_width = int(image.shape[1] * random.uniform(0.8, 1.5))
+    new_height = int(image.shape[0] * random.uniform(0.8, 1.5))
+    resized_image = cv2.resize(image, (new_width, new_height))
+    cv2.imwrite(output_path, resized_image)
+
+def affine_warp(input_path, output_path):
+    w = random.randint(20, 50)
+    h = random.randint(20, 50)
+    src_points = [[w, w], [w, h], [h, w]]
+    dst_points = [[random.randint(0, w*2), random.randint(0, h*2)] for _ in range(3)]
     args = []
-    for (sx, sy), (dx,dy) in zip(src_points, dst_points):
+    for (sx, sy), (dx, dy) in zip(src_points, dst_points):
         args.extend([sx, sy, dx, dy])
     with Image(filename=input_path) as img:
         img.virtual_pixel = 'white'
-        img.distort('affine' tuple(args))
-        img.save(filename=output_path )
-def perspective_warp_wand(input_path, output_path, src_points, dst_points):
+        img.distort('affine', tuple(args))
+        img.save(filename=output_path)
+
+def perspective_warp(input_path, output_path):
+    w = random.randint(20, 50)
+    h = random.randint(20, 50)
+    src_points = [[0, 0], [w, 0], [w, h], [0, h]]
+    dst_points = [[random.randint(0, w*2), random.randint(0, h*2)] for _ in range(4)]
     args = []
     for (sx, sy), (dx, dy) in zip(src_points, dst_points):
         args.extend([sx, sy, dx, dy])
@@ -50,18 +71,31 @@ def perspective_warp_wand(input_path, output_path, src_points, dst_points):
         img.virtual_pixel = 'white'
         img.distort('perspective', tuple(args))
         img.save(filename=output_path)
-def rotate.py(input_path, output_path, deg):
-    with Image(filename=input_path) as  img:
-        img.virtual_pixel = 'white'
-        img.rotate(angle)
-        img.save(filename=output_path)
-def translate_image_wand_simple(input_path, output_path, shift_x=0, shift_y=0):
-    with Image(filename=input_path) as img:
-        img.transform(resize=None, crop=None, translate=(shift_x, shift_y))
-        img.background_color = 'white'
-        img.save(filename=output_path)
-def scale(input_path, output_path, new_width, new_height):
-    image = cv2.imread(input_file)
-    resized_image = cv2.resize(image,(new_width, new_height))
-    cv2.imwrite(output_file)
-    
+
+def apply_random_distortions(input_folder, output_folder, samples_per_image):
+    os.makedirs(output_folder, exist_ok=True)
+    distortions = [radial, stretch, rotate, translate, scale, affine_warp, perspective_warp]
+
+    for root, dirs, _ in os.walk(input_folder):
+        if root == input_folder:
+            for subdir in dirs:
+                sub_input = os.path.join(root, subdir)
+                sub_output = os.path.join(output_folder, subdir)
+                os.makedirs(sub_output, exist_ok=True)
+
+                for file in os.listdir(sub_input):
+                    if file.lower().endswith(('.bmp', '.tif', '.tiff', '.png', '.jpg', '.jpeg')):
+                        input_path = os.path.join(sub_input, file)
+                        for i in range(samples_per_image):
+                            output_path = os.path.join(sub_output, f"{os.path.splitext(file)[0]}_dist{i}.bmp")
+                            chosen = random.sample(distortions, k=random.randint(1, 2))
+                            temp_path = input_path
+                            for j, distortion in enumerate(chosen):
+                                final_path = output_path if j == len(chosen) - 1 else temp_path + f"_tmp{j}.bmp"
+                                distortion(temp_path, final_path)
+                                temp_path = final_path
+
+input_folder = '/home/shanshal/Projects/Python/biometrics/image_processing/examples'
+output_folder = '/home/shanshal/Projects/Python/biometrics/image_processing/output/distorted_samples'
+
+apply_random_distortions(input_folder, output_folder, samples_per_image=1)
