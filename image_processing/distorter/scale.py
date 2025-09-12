867de20f569dcf7549531910eafec16db0c35717
shanshal <ahmedmahdii2003@gmail.com>
2025-09-12 17:30:32 +0300

    Remove comment blocks

--- image_processing/distorter/scale.py
+++ image_processing/distorter/scale.py
@@ -3,38 +3,9 @@ import cv2
 # Load the image
 image = cv2.imread("../examples/101_1.bmp")
  
-#######################################
-# # Display the original image        #
-# cv2.imshow("Original Image", image) #
-# cv2.waitKey(0)                      #
-# cv2.destroyAllWindows()             #
-#######################################
-# Define different scaling factors for width and height
-#fx = 0.088  
-#fy = 0.066
 new_width = 150
 new_height = 450
  
-
- # We might need this later i have no idea what this is but i found it online
-#############################################################
-#
-## Apply different interpolation methods
-# resized_area = cv2.resize(image, None, fx=fx, fy=fy, interpolation=cv2.INTER_AREA)
-# resized_linear = cv2.resize(image, None, fx=fx, fy=fy, interpolation=cv2.INTER_LINEAR)
-# resized_cubic = cv2.resize(image, None, fx=fx, fy=fy, interpolation=cv2.INTER_CUBIC)
-# resized_nearest = cv2.resize(image, None, fx=fx, fy=fy, interpolation=cv2.INTER_NEAREST)
-# Display the resized images                              #
-# cv2.imshow("Original Image", image)                       #
-# cv2.imshow("Resized with INTER_AREA", resized_area)       #
-# cv2.imshow("Resized with INTER_LINEAR", resized_linear)   #
-# cv2.imshow("Resized with INTER_CUBIC", resized_cubic)     #
-# cv2.imshow("Resized with INTER_NEAREST", resized_nearest) #
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-# Resizing #
-#
-#############################################################
 
 resized_image = cv2.resize(image, (new_width, new_height))
  
